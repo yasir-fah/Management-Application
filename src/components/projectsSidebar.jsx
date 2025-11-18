d8e68e85308a4e37147438ec6334dc40278ae63f
dev <dev@test.com>
2025-11-18 18:59:01 +0300

    fix an css issue, initilize new component

---
+++ src/components/projectsSidebar.jsx
@@ -0,0 +1,10 @@
+
+function projectsSidebar () {
+  <>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+
+    </aside>
+  </>
+}
+
+export default projectsSidebar;
