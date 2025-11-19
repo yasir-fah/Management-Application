199c100875f4eb0bad72d284aa1dfd240a0354d3
dev <dev@test.com>
2025-11-19 16:38:28 +0300

    Manage state and switch between 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import ProjectsSidebar from './components/ProjectsSidebar.jsx'
 import NewProject from './components/NewProject.jsx'
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
+import { useState } from 'react';
+
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-    <ProjectsSidebar />
-    <NewProject />
-    <NoProjectSelected />
-    </main>
-  );
+
+  const [newProject , setNewProject] = useState(false);
+
+  function handleNewProject() {
+    setNewProject(currentState => !currentState);
+  }
+
+ return (
+  <aside className='h-screen my-8 flex gap-8'>
+    <ProjectsSidebar addProject={handleNewProject} isCreate={newProject}/>
+    {newProject ? <NewProject /> : <NoProjectSelected  addProject={handleNewProject}/>}
+  </aside>
+ )
+
 }
 
 export default App;
--- src/components/projectsSidebar.jsx
+++ src/components/projectsSidebar.jsx
@@ -1,11 +1,11 @@
 
-function ProjectsSidebar () {
+function ProjectsSidebar ({addProject, isCreate}) {
   return (
     <>
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
       <div>
-        <button className="-full text-left px-2 py-2 text-xs md:text-base rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 bg-stone-600  ">+ Add New Project</button>
+        <button onClick={addProject} className="-full text-left px-2 py-2 text-xs md:text-base rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 bg-stone-600  ">{isCreate ? "Cancel" : "+ Add New Project" }</button>
       </div>
       <ul className="mt-8">
         {/* we will att the li & button dynamically depend on projects we have */}
